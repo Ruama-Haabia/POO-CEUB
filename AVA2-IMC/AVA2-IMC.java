8a70944e2238c34ce5ac63459c9f5586a29d24dc
Ruama Haabia <ruama.haabia@sempreceub.com>
2025-04-14 10:16:48 -0300

    Add files via upload
    AVA2-IMC-Analise do IMC

---
+++ AVA2-IMC/AVA2-IMC.java
@@ -0,0 +1,15 @@
+//classe de pessoas 
+puclic class Pessoa{
+    public static float calcularImcPessoa (float peso,float altura)
+        float peso = ;
+        float altura =;
+        float imc = peso/ (Mach pow(altura,2));
+        system.out.println("imc:" + imc);
+        return imc;  
+}
+// analise imc 
+public class pessoa{
+    public static float (analiseImc)
+    
+
+}
